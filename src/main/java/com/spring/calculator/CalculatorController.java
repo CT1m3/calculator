d05e611a540bed65b97812b5b66741cff7f1590f
CT1m3 <simas.banevicius26@gmail.com>
2024-03-21 16:59:01 +0200

    Not Working

--- src/main/java/com/spring/calculator/CalculatorController.java
+++ src/main/java/com/spring/calculator/CalculatorController.java
@@ -1,27 +1,17 @@
 package com.spring.calculator;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 // @RestController anotacija nurodo, jog Spring tipo rezultatas turetu buti isspausdinamas kleintui toks koks yra
 @RestController
-
+@Controller
 // Zymi konfihuracijos komponenta viduje leidzia kurti bean per metodus su @bean anotacija
 @EnableAutoConfiguration
 public class CalculatorController {
-    @RequestMapping("/skaiciuoti")
-    public int skaiciuoti(int num1, int num2, String symbol){
-        if(symbol.equals("-")){
-            return num1 - num2;
-        }
-        else if(symbol.equals("*")){
-            return num1 * num2;
-        }
-        else if(symbol.equals("/")){
-            return num1 / num2;
-        }
-        else {
-            return num1 + num2;
-        }
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String skaiciuotuvas() {
+        return "skaiciuotuvas";
     }
 }
